a32d52c6e951893729af0b53dfefec0312242dda
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-04 19:34:23 +0700

    :art: Fix weather view with dynamic City on code
    Fix weather view with dynamic City on code

--- src/WeatherService.js
+++ src/WeatherService.js
@@ -7,13 +7,9 @@ class WeatherServices {
     }
 
     
-    async sendRequset() {
+    async getByCity(city) {
         try {
-            const datas = await axios.get('https://wttr.in/Bantul?format=j1');
-            let realData = datas.data;
-            let {current_condition, nearest_area, request, weather} = realData;
-            this.currentCondition = current_condition;
-            this.weather = weather;
+            const datas = await axios.get(`https://wttr.in/${city}?format=j1`);
             return datas.data;
         } catch (err) {
             return err;
@@ -21,41 +17,12 @@ class WeatherServices {
 
     }
 
-    async getData() {
-        const data = await this.sendRequset();
-        return this.currentCondition;
+    async getDatas() {
+        const data = await this.sendRequest();
+        return data;
     }
 
-    async getCurrentCondition() {
-        await this.sendRequset()
-        return this.currentCondition;
-    }
-
-    async getWeather() {
-        await this.sendRequset();
-        console.log('get weather ' + this.weather)
-        return this.weather;
-    }
-
-    setLabel()
-    {
-        const data = {
-            labels: ['00', '02', '03', '04', '05', '06'],
-            datasets: [{
-              label: 'My First dataset',
-              backgroundColor: 'rgb(255, 99, 132)',
-              borderColor: 'rgb(255, 99, 132)',
-              data: [0, 10, 5, 2, 20, 30, 45],
-            }]
-          };
-    }
-
-    getWeather() {
-        return this.weather;
-    }
-
-
-
+    
 }
 
 
