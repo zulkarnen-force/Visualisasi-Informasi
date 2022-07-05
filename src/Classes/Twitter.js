4d41fd976830642d28140f8ad38c840991e1a42a
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-05 10:55:50 +0700

    add new classes Weather and Twitter

---
+++ src/Classes/Twitter.js
@@ -0,0 +1,148 @@
+class Twitter {
+
+    constructor (properties) {
+        const {status, data} = properties;
+        this.status = status;
+        this.data = data;
+    }
+
+    getData() {
+        return this.data;
+    }
+
+
+    getTrends() {
+        return this.data.trends;
+    }
+
+    getTrendData() {
+        const trends = this.getTrends();
+        let trendsData = []
+        for (let index = 0; index < trends.length; index++) {
+            trendsData.push(trends[index].data) 
+        }
+        return trendsData;
+    }
+
+    getNewTren() {
+        const newTren = this.getTrends()[0];
+        return newTren;
+    }
+
+    setConfigTren(config) {
+        const {data, labels} = config;
+        return  {
+            labels: labels,
+            datasets: [{
+                label: 'Trends Tweet Today',
+                data: data,
+                backgroundColor: [ 
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)',
+                    'rgb(234, 205, 86)',
+                    'rgb(255, 123, 86)',
+                    'rgb(255, 205, 23)',
+                    'rgb(255, 123, 86)',
+                ],
+                hoverOffset: 4
+            }]
+          };
+    }
+
+    setDataConfig(data){
+        return  {
+            type: 'doughnut', data: data,
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+                
+            }
+        };
+    }
+
+    getConfigTrend(trends = []) {
+        let labels = [];
+
+        for (const trend of trends) {
+            labels.push({labels: trend.name, data: (trend.tweet_count.length !== 0) ? parseInt(trend.tweet_count.slice(0, -1)) : 0});
+        }
+
+        let a = 0; 
+        for (const count of labels) {
+            a += count.data; 
+        }
+
+        const labelAndData = this.cleanNullNumber(labels, a/trends.length);
+        const dataConfig = this.setConfigTren(labelAndData)
+        return this.setDataConfig(dataConfig);
+    }
+
+    cleanNullNumber(datas = [], number) {
+        const labels = [];
+        const data = [];
+
+        for (const d of datas) {
+            labels.push(d.labels);
+            data.push((d.data) === 0 ? number : d.data);
+        }
+
+        return {labels, data};
+    }
+    onlyHasTweetCount() {
+        const trendsData = this.getTrendData();
+        let filtered = [];
+        for (let index = 0; index < trendsData.length; index++) {
+            let trendMap = trendsData[index].filter((value) => value.tweet_count.length !== 0);
+            filtered.push(trendMap);
+        }
+        return filtered;
+    }
+
+
+    getDataAndLabels() {
+        const datas = this.onlyHasTweetCount();
+        let data;
+        let labels;
+        for (let index = 0; index < datas.length; index++) {
+            data = datas[index].map( (item) => parseInt(item['tweet_count'].slice(0, -1)) );
+            labels = datas[index].map( (item) => item['name'] );
+        }
+        return {data, labels};
+    }
+
+    getDataConfig(){
+        const {data, labels} = this.getDataAndLabels();
+        return  {
+            labels: labels,
+            datasets: [{
+                label: 'Trends Tweet Today',
+                data: data,
+                backgroundColor: [ 
+                    'rgb(255, 99, 132)',
+                    'rgb(54, 162, 235)',
+                    'rgb(255, 205, 86)'
+                ],
+                hoverOffset: 4
+            }]
+          };
+    }
+
+
+    setConfig() {
+        return  {
+            type: 'doughnut', data: this.getDataConfig(), 
+            options: {
+                responsive: true,
+                maintainAspectRatio: false
+            }
+        };
+    }
+
+    getConfig(){
+        return this.setConfig();
+    }
+}
+
+
+module.exports = Twitter;
---
+++ src/Classes/Weather.js
@@ -0,0 +1,119 @@
+class Weather {
+    constructor (properties) {
+        let {current_condition, nearest_area, request, weather} = properties;
+        this.weather = weather;
+        this.currentCondition = current_condition;
+    }
+
+    getWeather() {
+        return this.weather;
+    }
+
+    getCurrent() {
+        return this.currentCondition;
+    }
+
+
+   
+
+
+
+
+    getCelciusHours () {
+        const stages = this.getHourly();
+        let celciusPerDay = [];
+        for (const stage of stages) {
+            for (const s of stage) {
+                celciusPerDay.push(parseInt(s['tempC']))
+            }
+        }
+        return celciusPerDay;   
+    }
+
+    setConfig() {
+        return {
+            type:'line',
+            data: {
+                labels: this.getLabel(),
+                datasets: [{
+                label: 'Suhu Udara Sleman',
+                backgroundColor: 'rgb(255, 99, 132)',
+                borderColor: 'rgb(255, 99, 132)',
+                data: this.getCelciusHours(),
+            }]},
+            options: {
+                scales: {
+                    y: {
+                        ticks: {
+                            // Include a dollar sign in the ticks
+                            callback: function(value, index, ticks) {
+                                return '$' + value;
+                            }
+                        }
+                    }
+                },
+                plugins: {
+                    title: {
+                        align: 'left',
+                        fullSize: true,
+                    }
+                },
+                devicePixelRatio: 10,
+                responsive: true,
+                layout: {
+                    padding: 1,
+                    autoPadding: false
+                },
+                animations: {
+                    onProgress: function(animation) {
+                        progress.value = animation.currentStep / animation.numSteps;
+                    }
+                },
+                transitions: {
+                  show: {
+                    animations: {
+                      x: {
+                        from: 0
+                      },
+                      y: {
+                        from: 0
+                      }
+                    }
+                  },
+                  hide: {
+                    animations: {
+                      x: {
+                        to: 0
+                      },
+                      y: {
+                        to: 0
+                      }
+                    }
+                  }
+                }
+              }
+          };
+    }
+
+    getHourly() {
+        let hourly = []; 
+        const weathers = this.getWeather();
+        for (const weather of weathers) {
+            hourly.push(weather['hourly'])
+        }
+        return hourly;
+    }
+
+    getLabel() {
+        let start = 0;
+        let labelHours = [];
+        for (let i = 0; i < 24; i++) {
+            labelHours.push((start+i).toString());
+        }
+
+        return labelHours;
+    }
+}
+
+
+module.exports = Weather;
