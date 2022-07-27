b6762c4f5d14300743b5afd4fd0ac83aebbf9ab9
Zulkarnen <zulkarnen1900016072@webmail.uad.ac.id>
2022-07-27 08:38:14 +0700

    fix routes controller issue

--- src/index.js
+++ src/index.js
@@ -1,7 +1,7 @@
 const express = require('express')
 const app = express();
 const path = require('path');
-const route = require('./controllers/route');
+const route = require('./Controllers/route');
 
 // app.use(express.static(path.join(__dirname, 'public')));
 
